8973f6fd0540cd5865a9cbc75d27ab3bf4da4f49
tato1599 <rndoctavio@gmail.com>
2023-10-02 01:28:36 -0600

    Inicio de proyecto

---
+++ app/Livewire/ComprensionLectora.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ComprensionLectora extends Component
+{
+    public function render()
+    {
+        return view('livewire.comprension-lectora');
+    }
+}
